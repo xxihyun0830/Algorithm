01000da9f63dfbcf0be1c58dabeb2e669982c4d7
LeeJiHyun <xxihyun0830@gmail.com>
2019-07-09 23:33:39 +0900

    initial commit

---
+++ Person.java
@@ -0,0 +1,16 @@
+package aa;
+
+public class Person {
+	
+	
+	public static void main(String[] args) {
+		String name;
+		int age;
+		
+		name = "cocomong";
+		age = 12;
+		
+		System.out.println(name + " :" + age);
+		
+	}
+}
